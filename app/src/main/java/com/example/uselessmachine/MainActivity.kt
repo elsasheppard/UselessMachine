b55055be5d15b275b8e577bd32d9ac7c2e92f6fc
Elsa Sheppard <sheppard.elsa03@gmail.com>
2020-09-03 13:20:28 -0700

    Continue writing core functions

--- app/src/main/java/com/example/uselessmachine/MainActivity.kt
+++ app/src/main/java/com/example/uselessmachine/MainActivity.kt
@@ -11,28 +11,59 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // When you see View with a capital V, what kind of Kotlin entity does that indicate? --> class
         // View.OnClickListener
-        // a class inside of a class?
-        // What else could be capital in Java? --> classes and interfaces
-        // can you construct and Interface? --> no
-        // val listener = View.OnClickListener() --> doesn't work
-        // you can make a class that implements and interface
-        // we could make a class that implements View.OnClickListener and defines how onClick(v: View!) behaves
+        // lambda --> anonymous function
+        // you can use a lambda to implement a one-function interface
+        // onClick(view: View) is the function being implemented by the lambda
 
-        // Kotlin has a feature called a lambda which lets you make a function without a name that can actually stand in for
-        // an interface with only one function in it
+        // when there is one parameter in the function, "it" is used to refer to that parameter
+        // ${} is a string concatenation template
 
-        // the one function that View.OnClickListener has is onClick(v: View!)
-        // this lambda below is implementing that one function onClick without really mentioning it explicitly
-        // the one parameter is referenced by "it". So to access that view, I can use "it"
         button_main_look_busy.setOnClickListener {
             Toast.makeText(this, "Hello, this is the text on the button ${(it as Button).text.toString()}", Toast.LENGTH_SHORT).show()
-            // ${} is the same as + concatenation + in java
-            // it as Button --> casting the variable it into a Button
-            // the it:View isn't typed, it's a tooltip that tells you that it's available
-            // when you hit enter between {} moving them to different lines, the tooltip showed up
+
         }
+
+        // to listen to a switch, you can use the OnCheckedChangeListener
+        // rely on documentation a lot in this class
+
+
+        switch_main_useless.setOnCheckedChangeListener { compoundButton, isChecked ->
+            // 1. toast the status of the button (checked, or not checked)
+
+            // can assign the result of an if statement as a val
+            // only in Kotlin
+            val message = if(isChecked) "Switch is ON" else "Switch is OFF"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            // could also do:
+            // Toast.makeText(this, "The button is: ${isChecked.toString()}", Toast.LENGTH_SHORT).show()
+
+            // 2. if the button is checked, uncheck it
+            if(isChecked) {
+                // ideally, we wait a little bit of time, and then have this code execute
+                // but Thread.sleep is illegal on the main.UI thread
+                // every 15 milliseconds the android screen refreshes
+
+                // janky app --> when it doesn't really respond to your clicks and taps immediately
+                // if you do a Thread.sleep without specifying which thread, it assumes UI thread, and stops EVERYTHING on the app
+                // people would think their phone is freezing and bad
+                // have to keep the UI running smoothly
+
+                // CountDownTimer is effectively making a separate thread to keep track of the time
+                // we're going to make an anon inner class using CountDownTimer
+                // anon --> we're not naming the class... just implementing the methods
+                // it's a one-time use thing used right here
+
+                switch_main_useless.isChecked = false
+                // could also do compoundButton.toggle()
+                // could lead to spam click problems
+                // a thousand monkeys --> thousands of clicks all over your app to see if it breaks it
+            }
+        }
+
+        // look up the CountDownTimer API and see what is needed to implement your own custom timer
+        // How do you start it? How do you stop it? How do you do things when it's done?
+        // How do you do things while counting down?
 
     }
 }
