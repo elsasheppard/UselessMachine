ef203d8742c536290e2b3744d1f9564a3f577b69
Elsa Sheppard <sheppard.elsa03@gmail.com>
2020-09-09 13:15:53 -0700

    Add beginnings of CountDownTimer

--- app/src/main/java/com/example/uselessmachine/MainActivity.kt
+++ app/src/main/java/com/example/uselessmachine/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.uselessmachine
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+// MainActivity extends AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,16 +57,47 @@ class MainActivity : AppCompatActivity() {
                 // anon --> we're not naming the class... just implementing the methods
                 // it's a one-time use thing used right here
 
-                switch_main_useless.isChecked = false
-                // could also do compoundButton.toggle()
-                // could lead to spam click problems
-                // a thousand monkeys --> thousands of clicks all over your app to see if it breaks it
+                // class w/o a name --> anonymous inner class
+
+                // making an anon inner class saying this object extends CountDownTimer
+                val uncheckTimer = object: CountDownTimer(5000, 1000) {
+                    // red underline under object
+                        // implement members
+                    // members --> things that belong to a class
+
+                    // use for changing bg color
+                    private var currentColor = 0
+                    private var difference = 1L // L makes it a long var type instead of int
+
+                    // TODO
+                    // 1. Switch
+                        // a. randomize the time so it doesn't always turn off at a fixed interval
+                        // b. if the switch is manually turned off early, we cancel the timer
+                                // so if it gets turned back on we don't gave multiple timers running simultaneously
+                    // 2. Self-Destruct Button
+                        // a. 10 second countdown timer to display on the button
+                        // b. when the timer is up, call the finish() method to close activity
+                        // c. lock out the button by setting its wnabled attribute to false
+                            // make the button no longer clickable
+                        // d. get the screen to flash a different color at every instance of an interval
+                        // e. get the screen to flash faster the less time is remaining in the countdown
+
+
+                    override fun onFinish() {
+                        // change ConstraintLayout name to layout_main
+                        layout_main.setBackgroundColor(Color.rgb((0...255).random(), (0...255).random(), (0...255).random()))
+
+                        switch_main_useless.isChecked = false
+                        // when done, turn off
+                    }
+
+                    // p0 --> millisRemaining
+                    override fun onTick(millisRemaining: Long) {
+                        // you can have an empty function if you don't need to use it
+                    }
+                }
+                uncheckTimer.start()
             }
         }
-
-        // look up the CountDownTimer API and see what is needed to implement your own custom timer
-        // How do you start it? How do you stop it? How do you do things when it's done?
-        // How do you do things while counting down?
-
     }
 }
