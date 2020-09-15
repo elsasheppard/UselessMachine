66e2fed1b2096d7a52886db2134d98af7c5d3d82
Elsa Sheppard <sheppard.elsa03@gmail.com>
2020-09-14 21:56:39 -0700

    Build working self destruct button w/ flash

--- app/src/main/java/com/example/uselessmachine/MainActivity.kt
+++ app/src/main/java/com/example/uselessmachine/MainActivity.kt
@@ -43,30 +43,11 @@ class MainActivity : AppCompatActivity() {
 
             // 2. if the button is checked, uncheck it
             if(isChecked) {
-                // ideally, we wait a little bit of time, and then have this code execute
-                // but Thread.sleep is illegal on the main.UI thread
-                // every 15 milliseconds the android screen refreshes
 
-                // janky app --> when it doesn't really respond to your clicks and taps immediately
-                // if you do a Thread.sleep without specifying which thread, it assumes UI thread, and stops EVERYTHING on the app
-                // people would think their phone is freezing and bad
-                // have to keep the UI running smoothly
-
-                // CountDownTimer is effectively making a separate thread to keep track of the time
-                // we're going to make an anon inner class using CountDownTimer
-                // anon --> we're not naming the class... just implementing the methods
-                // it's a one-time use thing used right here
-
-                // class w/o a name --> anonymous inner class
-
-                // making an anon inner class saying this object extends CountDownTimer
                 val uncheckTimer = object: CountDownTimer(5000, 1000) {
-                    // red underline under object
-                        // implement members
-                    // members --> things that belong to a class
 
                     // use for changing bg color
-                    private var currentColor = 0
+                    private var isRed = false
                     private var difference = 1L // L makes it a long var type instead of int
 
                     // TODO
@@ -74,30 +55,64 @@ class MainActivity : AppCompatActivity() {
                         // a. randomize the time so it doesn't always turn off at a fixed interval
                         // b. if the switch is manually turned off early, we cancel the timer
                                 // so if it gets turned back on we don't gave multiple timers running simultaneously
-                    // 2. Self-Destruct Button
-                        // a. 10 second countdown timer to display on the button
-                        // b. when the timer is up, call the finish() method to close activity
-                        // c. lock out the button by setting its wnabled attribute to false
-                            // make the button no longer clickable
-                        // d. get the screen to flash a different color at every instance of an interval
-                        // e. get the screen to flash faster the less time is remaining in the countdown
+                    // 3. Add a progress bar that's hidden initially (visibility attribute)
 
 
                     override fun onFinish() {
                         // change ConstraintLayout name to layout_main
-                        layout_main.setBackgroundColor(Color.rgb((0...255).random(), (0...255).random(), (0...255).random()))
-
+                        layout_main.setBackgroundColor(Color.rgb((0..255).random(), (0..255).random(), (0..255).random()))
                         switch_main_useless.isChecked = false
                         // when done, turn off
                     }
 
                     // p0 --> millisRemaining
                     override fun onTick(millisRemaining: Long) {
-                        // you can have an empty function if you don't need to use it
+                        if(switch_main_useless.isChecked) {
+                            cancel()
+                        }
+                        if(isRed) {
+                            layout_main.setBackgroundColor(Color.rgb(255, 0, 0))
+                        }
+                        isRed = !isRed
                     }
                 }
                 uncheckTimer.start()
             }
+
+
+        }
+
+        button_main_self_destruct.setOnClickListener {
+            // 2. Self-Destruct Button
+            // a. 10 second countdown timer to display on the button
+            // b. when the timer is up, call the finish() method to close activity
+            // c. lock out the button by setting its enabled attribute to false
+            // make the button no longer clickable
+            // d. get the screen to flash a different color at every instance of an interval
+            // e. get the screen to flash faster the less time is remaining in the countdown
+
+            val uncheckTimer = object: CountDownTimer(5000, 500) {
+
+                // use for changing bg color
+                private var isRed = false
+                private var difference = 1L // L makes it a long var type instead of int
+
+                override fun onFinish() {
+                    finish()
+                }
+
+                // p0 --> millisRemaining
+                override fun onTick(millisRemaining: Long) {
+                    if(isRed) {
+                        layout_main.setBackgroundColor(Color.rgb(255, 0, 0))
+                    }
+                    else {
+                        layout_main.setBackgroundColor(Color.rgb(255, 255, 255))
+                    }
+                    isRed = !isRed
+                }
+            }
+            uncheckTimer.start()
         }
     }
 }
